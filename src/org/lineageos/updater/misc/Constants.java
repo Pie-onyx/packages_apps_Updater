3f83ba1a396160fe3e3921fcc01f9ec4d6b51498
YumeMichi <do4suki@gmail.com>
2018-06-23 13:17:08 +0800

    Updater: OmniROM bringup
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -32,12 +32,12 @@ public final class Constants {
     public static final String UNCRYPT_FILE_EXT = ".uncrypt";
 
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
-    public static final String PROP_BUILD_VERSION = "ro.aosp.version";
+    public static final String PROP_BUILD_VERSION = "ro.omni.build.version";
     public static final String PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental";
-    public static final String PROP_DEVICE = "ro.lineage.device";
+    public static final String PROP_DEVICE = "ro.omni.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_RELEASE_TYPE = "ro.aosp.releasetype";
-    public static final String PROP_UPDATER_URI = "aosp.updater.uri";
+    public static final String PROP_RELEASE_TYPE = "ro.omni.releasetype";
+    public static final String PROP_UPDATER_URI = "updater.uri";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
