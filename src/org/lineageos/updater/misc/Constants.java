39c26b8e651cd57e0dd7bb84e0062e5207ccc641
YumeMichi <do4suki@gmail.com>
2018-09-17 01:30:54 +0800

    Updater: LemonPie bringup
    Change-Id: I002d75e4946cca7087026956fe14d8ba78a97d7e
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -32,11 +32,11 @@ public final class Constants {
     public static final String UNCRYPT_FILE_EXT = ".uncrypt";
 
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
-    public static final String PROP_BUILD_VERSION = "ro.omni.build.version";
+    public static final String PROP_BUILD_VERSION = "ro.lemon.build.version";
     public static final String PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental";
-    public static final String PROP_DEVICE = "ro.omni.device";
+    public static final String PROP_DEVICE = "ro.lemon.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_RELEASE_TYPE = "ro.omni.releasetype";
+    public static final String PROP_RELEASE_TYPE = "ro.lemon.releasetype";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "updater.uri";
 
